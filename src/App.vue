fcdbaa6d28bbf6987a7aa2b58ab2245f737f80a1
John Sultan <jenya.sultan@yandex.ru>
2024-03-01 22:59:05 +0300

    add imeg

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <h1>Template</h1>
+  <div class="w-4/5">
+    <h1>Sultan Yevgenii</h1>
+  </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
 <style scoped></style>
