deeaba4b3f791a01e0314d575bf39ae345d3189d
John Sultan <jenya.sultan@yandex.ru>
2024-03-01 17:09:56 +0300

    init repo

---
+++ src/components/MyHello.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="product">
+    <h2>Товар: {{ title }}</h2>
+    <h3>Цена: {{ price }}</h3>
+    <h3>Добавлен: {{ isAdded }}</h3>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String
+  },
+  price: {
+    type: Number
+  },
+  isAdded: {
+    type: Boolean
+  }
+})
+</script>
+
+<style scoped>
+.product {
+  border: 1px solid blue;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
