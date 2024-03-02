f14bbcb5892b0128e2b79b4e83ff41f1978985ef
John Sultan <jenya.sultan@yandex.ru>
2024-03-02 21:52:14 +0300

    add click plus and like

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,12 @@
   <div
     class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition"
   >
-    <img src="/like-1.svg" alt="like 1" class="absolute top-8 left-8" />
+    <img
+      @click="onFavoriteAdd"
+      :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
+      alt="like"
+      class="absolute top-8 left-8"
+    />
     <img :src="imageUrl" alt="sneakers-1" />
     <p class="mt-2" v-text="title"></p>
     <div class="flex justify-between mt-5">
@@ -10,7 +15,11 @@
         <span class="text-slate-400">Цена:</span>
         <b>{{ price }} руб.</b>
       </div>
-      <img src="/plus.svg" alt="plus" />
+      <img
+        @click="onClickAdd"
+        :src="!isAdded ? '/plus.svg' : '/checked.svg'"
+        :alt="isAdded ? 'plus' : 'checked'"
+      />
     </div>
   </div>
 </template>
@@ -19,6 +28,10 @@
 defineProps({
   imageUrl: String,
   title: String,
-  price: Number
+  price: Number,
+  isFavorite: Boolean,
+  isAdded: Boolean,
+  onClickAdd: Function,
+  onFavoriteAdd: Function
 })
 </script>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <Card image-url="/sneakers/sneakers-1.jpg" title="Мужские кроссовки Nike Blazer Mid Suede" :price="12990"/>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Card
+      image-url="/sneakers/sneakers-1.jpg"
+      title="Мужские кроссовки Nike Blazer Mid Suede"
+      :price="12990"
+      :is-added="true"
+      :is-favorite="true"
+      :onClickAdd="onClickAdd"
+      :onFavoriteAdd="onFavoriteAdd"
+    />
   </div>
 </template>
 
 <script setup>
 import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('Add')
+}
+const onFavoriteAdd = () => {
+  alert('Favorite')
+}
 </script>
