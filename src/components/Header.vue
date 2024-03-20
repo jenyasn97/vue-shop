2f0aebf229acc4e4536c0e786281c7101eb34cc5
John Sultan <jenya.sultan@yandex.ru>
2024-03-20 08:54:38 +0300

    fix router

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,14 @@
 <template>
   <header class="flex justify-between border-b border-slate-200 px-10 py-8">
-    <div class="flex items-center gap-4">
-      <img src="/logo.png" alt="Logo" class="w-10" />
-      <div>
-        <RouterLink to="/"
-          ><h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
-          <p class="text-slate-400">Магазин лучшеших кроссовок</p></RouterLink
-        >
+    <RouterLink to="/">
+      <div class="flex items-center gap-4">
+        <img src="/logo.png" alt="Logo" class="w-10" />
+        <div>
+          <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+          <p class="text-slate-400">Магазин лучшеших кроссовок</p>
+        </div>
       </div>
-    </div>
+    </RouterLink>
 
     <ul class="flex items-center gap-10">
       <li
